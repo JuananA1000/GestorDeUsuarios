1545d17a1f6941f8ce3bb8476ee3b17720595c00
Juan Antonio <juanantonioamillopez@gmail.com>
2022-01-21 14:12:43 +0100

    Componente de Input

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
+import Input from "./components/Input";
+
 // Custom HOOKS
 import useFormulario from "./hooks/useFormulario";
 
 function App() {
-    const [formulario, handleChange] = useFormulario({ name: "" });
+    const [formulario, handleChange] = useFormulario({
+        nombre: "",
+        apellido: "",
+    });
     console.log(formulario);
 
     return (
         <form>
-            <input
-                name="name"
-                placeholder="Nombre"
-                value={formulario.name}
+            <Input
+                label="Nombre"
+                name="nombre"
+                value={formulario.nombre}
+                onChange={handleChange}
+            />
+
+            <Input
+                label="Apellido"
+                name="apellido"
+                value={formulario.apellido}
                 onChange={handleChange}
             />
         </form>
