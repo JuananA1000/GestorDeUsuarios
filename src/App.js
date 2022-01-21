4a88dcbbd6920829c3a003123b81f05620ccf941
Juan Antonio <juanantonioamillopez@gmail.com>
2022-01-21 13:24:39 +0100

    Primeros pasos y custom hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+// Custom HOOKS
+import useFormulario from "./hooks/useFormulario";
+
 function App() {
-    return <p>Gestor de Usuarios</p>;
+    const [formulario, handleChange] = useFormulario({ name: "" });
+    console.log(formulario);
+
+    return (
+        <form>
+            <input
+                name="name"
+                placeholder="Nombre"
+                value={formulario.name}
+                onChange={handleChange}
+            />
+        </form>
+    );
 }
 
 export default App;
